6379cbcc284df2d6c55156a4d2f0fca3ad349c97
GuerinTifenn <8807@holbertonstudents.com>
2024-05-10 10:53:06 +0200

    up

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     argc = len(sys.argv) - 1
     if argc == 0:
         print("{} argument.".format(argc))
-    if argc == 1:
+    elif argc == 1:
         print("{} argument.".format(argc))
     else:
         print("{} arguments:".format(argc))
