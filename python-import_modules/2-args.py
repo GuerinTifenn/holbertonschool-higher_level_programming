a058c9edd4c156da77b60f2de9d148eece85140d
GuerinTifenn <8807@holbertonstudents.com>
2024-05-10 10:25:11 +0200

    add format

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -4,9 +4,10 @@ from sys import argv
 argc = len(argv) - 1
 
 if __name__ == "__main__":
-    if argc == 0:
-        print("0 arguments.")
-    elif argc == 1:
-        print("1 argument:")
+    if argc <= 1:
+        print("{} argument.".format(argc))
     else:
         print("{} arguments:".format(argc))
+
+    for i in range(1, argc + 1):
+        print("{}: {}".format(i, argv[i]))
