c0b8242b9538e49000d072b197b010e49fd8a9f8
GuerinTifenn <8807@holbertonstudents.com>
2024-05-10 10:55:03 +0200

    update

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -4,10 +4,10 @@ if __name__ == "__main__":
     import sys
     argc = len(sys.argv) - 1
     if argc == 0:
-        print("{} argument.".format(argc))
-    elif argc == 1:
-        print("{} argument.".format(argc))
-    else:
+        print("0 argument.")
+    if argc == 1:
+        print("1 argument.")
+    if argc > 1:
         print("{} arguments:".format(argc))
     for i in range(1, argc + 1):
         print("{}: {}".format(i, sys.argv[i]))
