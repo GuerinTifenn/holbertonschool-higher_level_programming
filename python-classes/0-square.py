403aba08e547ded50b5566c58965159a3313da93
GuerinTifenn <8807@holbertonstudents.com>
2024-05-17 11:11:11 +0200

    add task 0

---
+++ python-classes/0-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""
+This Python script defines a class named Square.
+"""
+class Square:
+    """
+    This class is defined with no attributes or methods.
+    The "pass" statement is used to indicate that the class is empty for now.
+    """
+    pass
