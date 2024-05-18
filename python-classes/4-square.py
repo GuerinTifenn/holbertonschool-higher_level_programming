6aefc0cab8d8b060d3807496a31aa27a129481fc
GuerinTifenn <8807@holbertonstudents.com>
2024-05-18 11:13:49 +0200

    add task

---
+++ python-classes/4-square.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+"""
+This module defines an empty class named Square.
+"""
+
+
+class Square:
+    """
+    This class defines a square.
+
+    Attributes:
+        __size (int): Private instance attribute representing size of square.
+    """
+
+    def __init__(self, size=0):
+        """
+        Initializes a Square instance with an optional size.
+
+        Args:
+            size (int): The size of the square. Defaults to 0.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """
+        Getter method for retrieving the size of the square.
+
+        Returns:
+            int: The size of the square.
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        Setter method for setting the size of the square.
+
+        Args:
+            value (int): The size value to be set.
+
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is less than 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """
+        Calculates the area of the square.
+
+        Returns:
+            int: The area of the square.
+        """
+        return self.__size ** 2
