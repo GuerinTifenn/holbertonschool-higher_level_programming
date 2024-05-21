f4cb07db03a1c5dd2129fc9195d17936c89b141c
GuerinTifenn <8807@holbertonstudents.com>
2024-05-21 10:03:56 +0200

    add a comment at task 3

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,6 +1,17 @@
 #!/usr/bin/python3
 
 def say_my_name(first_name, last_name=""):
+    """
+    Prints a square with the character '#'.
+
+    Args:
+        size (int): The size length of the square's side.
+        Must be a non-negative integer.
+
+    Raises:
+        TypeError: If size is not an integer.
+        ValueError: If size is less than 0.
+    """
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
