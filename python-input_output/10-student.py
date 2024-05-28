8822f4e4520808c533540870f176724845856728
GuerinTifenn <8807@holbertonstudents.com>
2024-05-28 09:29:51 +0200

    add task 10

---
+++ python-input_output/10-student.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""student to JSON"""
+
+class Student:
+    """
+    A class that defines a student by first name, last name, and age.
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializes the student with first name, last name, and age.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Retrieves a dictionary representation of a Student instance.
+        If attrs is a list of strings, only attributes names contained
+        in this list must be retrieved. Otherwise, all attributes must
+        be retrieved.
+        """
+        if attrs is None:
+            return self.__dict__
+        else:
+            result = {}
+            for attr in attrs:
+                if hasattr(self, attr):
+                    result[attr] = getattr(self, attr)
+            return result
