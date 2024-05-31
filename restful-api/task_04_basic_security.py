afbaddd63c6bc5ea25a995b9d515056a4ea3d160
GuerinTifenn <8807@holbertonstudents.com>
2024-05-31 14:31:18 +0200

    update task 4

--- restful-api/task_04_basic_security.py
+++ restful-api/task_04_basic_security.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """Develop a Simple API using Python with Flask"""
-
 from flask import Flask, jsonify, request
 
 
@@ -23,43 +22,43 @@ users = {
 }
 
 
-@app.route('/')
+@app.route("/")
 def home():
     """Route for the root URL"""
     return "Welcome to the Flask API!"
 
 
-@app.route('/data')
+@app.route("/data")
 def get_data():
     """Route to serve JSON data"""
     return jsonify(list(users.keys()))
 
 
-@app.route('/status')
+@app.route("/status")
 def get_status():
     """Route to return OK status"""
     return "OK"
 
 
-@app.route('/users/<username>')
+@app.route("/users/<username>")
 def get_user(username):
     """Route to return user data by username"""
-    if username in users:
-        return jsonify(users[username])
+    user = users.get(username)
+    if user:
+        return jsonify(user)
     else:
         return jsonify({"error": "User not found"}), 404
 
 
-@app.route('/add_user', methods=['POST'])
+@app.route("/add_user", methods=["POST"])
 def add_user():
     """Route to add a new user"""
-    data = request.json
-    username = data.get('username')
-    if username:
-        users[username] = data
-        return jsonify({"message": "User added", "user": data}), 201
-    else:
-        return jsonify({"error": "Username not provided"}), 400
+    user_data = request.get_json()
+    username = user_data.get("username")
+    if not username:
+        return jsonify({"error": "Username is required"}), 400
+    users[username] = user_data
+    return jsonify({"message": "User added", "user": user_data}), 201
 
 
 if __name__ == "__main__":
