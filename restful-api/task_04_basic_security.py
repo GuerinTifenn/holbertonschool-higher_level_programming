f5c5b06d85aa9778cf790f8da318c75f1ac30a37
GuerinTifenn <8807@holbertonstudents.com>
2024-05-31 14:36:56 +0200

    update task 4

--- restful-api/task_04_basic_security.py
+++ restful-api/task_04_basic_security.py
@@ -1,28 +1,48 @@
 #!/usr/bin/python3
+"""Develop a Simple API using Python with Flask"""
 from flask import Flask, jsonify, request
+
 
 app = Flask(__name__)
 
-users = {}
+# Sample user data stored in memory
+users = {
+    "jane": {
+        "username": "jane",
+        "name": "Jane",
+        "age": 28,
+        "city": "Los Angeles"
+    },
+    "john": {
+        "username": "john",
+        "name": "John",
+        "age": 30,
+        "city": "New York"
+    }
+}
 
 
 @app.route("/")
 def home():
+    """Route for the root URL"""
     return "Welcome to the Flask API!"
 
 
 @app.route("/data")
 def get_data():
+    """Route to serve JSON data"""
     return jsonify(list(users.keys()))
 
 
 @app.route("/status")
-def status():
+def get_status():
+    """Route to return OK status"""
     return "OK"
 
 
 @app.route("/users/<username>")
 def get_user(username):
+    """Route to return user data by username"""
     user = users.get(username)
     if user:
         return jsonify(user)
@@ -32,6 +52,7 @@ def get_user(username):
 
 @app.route("/add_user", methods=["POST"])
 def add_user():
+    """Route to add a new user"""
     user_data = request.get_json()
     username = user_data.get("username")
     if not username:
