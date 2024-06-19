c73c6f97fe25e8737467bc24bfed730169244a2a
GuerinTifenn <8807@holbertonstudents.com>
2024-06-19 17:02:08 +0200

    add others tasks

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,1 +1,3 @@
-USE hbtn_0d_usa; CREATE TABLE IF NOT EXISTS cities (id INT AUTO_INCREMENT UNIQUE NOT NULL PRIMARY KEY, state_id INT NOT NULL, name VARCHAR(256) NOT NULL, FOREIGN KEY (state_id) REFERENCES states(id));
+-- Create the table cities with a FOREIGN KEY that references the states table
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities (id INT AUTO_INCREMENT UNIQUE NOT NULL PRIMARY KEY, state_id INT NOT NULL, name VARCHAR(256) NOT NULL, FOREIGN KEY (state_id) REFERENCES states(id));
