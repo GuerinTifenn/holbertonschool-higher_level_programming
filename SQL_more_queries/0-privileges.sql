b415cba4e64c280ecbe7c3670b4ab2012e2748fe
GuerinTifenn <8807@holbertonstudents.com>
2024-06-19 13:05:56 +0200

    update task 0

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,16 +1,3 @@
--- Create user user_0d_1 if not exists
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
--- Grant all privileges to user_0d_1
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
-
--- Create user user_0d_2 if not exists
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
--- Grant select privilege to user_0d_2
-GRANT SELECT ON *.* TO 'user_0d_2'@'localhost';
-
--- Flush privileges to ensure all changes are applied
-FLUSH PRIVILEGES;
-
 -- Show grants for user_0d_1
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 
