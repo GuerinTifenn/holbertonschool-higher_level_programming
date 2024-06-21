c5e74d421c5797b91ddce95b0517e63777c50da9
GuerinTifenn <8807@holbertonstudents.com>
2024-06-21 16:49:16 +0200

    add all project

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -8,10 +8,12 @@ import sys
 
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306,
-                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+                         user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY id ASC"
-                .format(sys.argv[4]))
+    query = ("SELECT * FROM states WHERE name LIKE BINARY '{}' "
+             "ORDER BY id ASC".format(sys.argv[4]))
+    cur.execute(query)
     rows = cur.fetchall()
     for row in rows:
         print(row)
--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-Contains the class definition of a State and an instance Base = declarative_base().
+Contains the class definition of a State and an instance
+Base = declarative_base().
 """
 from sqlalchemy import Column, Integer, String, create_engine
 from sqlalchemy.ext.declarative import declarative_base
@@ -17,5 +18,6 @@ class State(Base):
     name = Column(String(128), nullable=False)
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
